18a4ae68b2aa76f42d90d5eb4f190345ea4217c6
Roman <pugachev.ra@yandex.ru>
2024-10-10 19:52:12 +0300

    Add python

--- SimpleFlaskAPI/SimpleFlaskAPI.py
+++ SimpleFlaskAPI/SimpleFlaskAPI.py
@@ -15,5 +15,4 @@ def home():
 
 if __name__ == "__main__":
     port = int(os.getenv("FLASK_APP_PORT", 8080))
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
