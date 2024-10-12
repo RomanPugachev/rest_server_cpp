2dab9b740ef23a966eade43a7c39358b807024f2
Roman <pugachev.ra@yandex.ru>
2024-10-12 13:23:17 +0300

    Services deploy with dry-stack

--- SimpleFlaskAPI/SimpleFlaskAPI.py
+++ SimpleFlaskAPI/SimpleFlaskAPI.py
@@ -1,9 +1,28 @@
 import os
-from os import times
-
+from flask import Flask
+from opentelemetry import trace
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.resources import Resource
+import logging
+import os
 from flask import Flask, jsonify, render_template
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+if os.environ.get('PYTEST_VERSION'):
+    os.environ['OTEL_SDK_DISABLED'] = 'true'
+
+trace.set_tracer_provider(TracerProvider())
+tracer_provider = trace.get_tracer_provider()
+otlp_exporter = OTLPSpanExporter()
+span_processor = BatchSpanProcessor(otlp_exporter)
+tracer_provider.add_span_processor(span_processor)
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
 print("*" * 50)
 print("Hello from application", end = " ")
 print(app)
@@ -11,6 +30,7 @@ print("*" * 50)
 
 @app.route('/')
 def home():
+    logging.info("Your application accepted a request!")
     return render_template('index.html')
 
 if __name__ == "__main__":
