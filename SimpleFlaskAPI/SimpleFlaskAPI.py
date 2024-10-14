6757d8a9aa6e93bedf1bbc85de3c5ca82eb29c89
Roman <pugachev.ra@yandex.ru>
2024-10-14 16:42:44 +0300

    Enabling opentelemetry: Added psycopg2 for correct working of opentelemetry

--- SimpleFlaskAPI/SimpleFlaskAPI.py
+++ SimpleFlaskAPI/SimpleFlaskAPI.py
@@ -1,9 +1,10 @@
 import os
+import psycopg2
 from flask import Flask
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.sdk.environment_variables import OTEL_RESOURCE_ATTRIBUTES
-# from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
+from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -27,6 +28,7 @@ if __name__ == "__main__":
     resource = Resource(attributes={k.strip(): v.strip() for k, v in (pair.split('=') for pair in otel_resource_attributes.split(','))})
     trace.set_tracer_provider(TracerProvider(resource=resource))
     tracer_provider = trace.get_tracer_provider()
+    # Set up the exporter
     otlp_exporter = OTLPSpanExporter(endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otlp_collector:4318/"))
     span_processor = BatchSpanProcessor(otlp_exporter)
     tracer_provider.add_span_processor(span_processor)
