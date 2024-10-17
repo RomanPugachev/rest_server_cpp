e5cc78154997d0b43de17447c1dfe32647bdfe94
Roman <pugachev.ra@yandex.ru>
2024-10-17 14:02:12 +0300

    Repair configuration

--- SimpleFlaskAPI/SimpleFlaskAPI.py
+++ SimpleFlaskAPI/SimpleFlaskAPI.py
@@ -1,8 +1,9 @@
 import os
+from cgitb import reset
 from datetime import datetime
 
 import psycopg2
-from flask import Flask
+from flask import Flask, request
 import time
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -15,7 +16,6 @@ from opentelemetry.sdk.resources import Resource
 import logging
 import os
 from flask import Flask, jsonify, render_template
-
 from Utils import get_docker_metadata
 
 app = Flask(__name__)
@@ -25,11 +25,12 @@ accepted_requests_count = 0
 
 @app.route('/')
 def home():
-    logging.info("Your application accepted a request!")
+    logging.info("Your application accepted a request on homepage!")
     return render_template('index.html')
 
 @app.route('/container_info')
 def container_info():
+    logging.info("Your application accepted a request on resource /container_info!")
     global accepted_requests_count, start_time
     accepted_requests_count+=1
     # Read container ID from /proc/self/cgroup (works inside Docker containers)
@@ -47,11 +48,11 @@ def container_info():
     container_id_api, replica_id, app_version = get_docker_metadata()
     # Collect information into a dictionary
     info = {
-        'service_container_uptime' : datetime.now() - start_time,
+        'service_container_uptime' : (datetime.now() - start_time).seconds,
         'accepted_requests': accepted_requests_count,
-        'container_id': container_id,
+        'container_id': container_id_api,
         'replica_id': replica_id,
-        'app_version': app_version,
+        'app_version': app_version
     }
     return jsonify(info)
 
@@ -61,29 +62,18 @@ if __name__ == "__main__":
         os.environ['OTEL_SDK_DISABLED'] = 'true'
 
     # Set tracer proovider
-    otel_resource_attributes = os.getenv("OTEL_RESOURCE_ATTRIBUTES", "deployment.environment=dev-local,service.name=flask")
-    resource = Resource(attributes={k.strip(): v.strip() for k, v in (pair.split('=') for pair in otel_resource_attributes.split(','))})
-    trace.set_tracer_provider(TracerProvider(resource=resource))
-    tracer_provider = trace.get_tracer_provider()
-    # Set up the exporter
-
-    otlp_exporter = OTLPSpanExporter(endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otlp_collector:4318/v1/traces"))
-    span_processor = BatchSpanProcessor(otlp_exporter)
-    tracer_provider.add_span_processor(span_processor)
+    #otel_resource_attributes = os.getenv("OTEL_RESOURCE_ATTRIBUTES", "deployment.environment=dev-local,service.name=flask")
+    #resource = Resource(attributes={k.strip(): v.strip() for k, v in (pair.split('=') for pair in otel_resource_attributes.split(','))})
+    #trace.set_tracer_provider(TracerProvider(resource=resource))
+    #tracer_provider = trace.get_tracer_provider()
+    ## Set up the exporter
+#
+    #otlp_exporter = OTLPSpanExporter(endpoint=os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://otlp_collector:4318/v1/traces"))
+    #span_processor = BatchSpanProcessor(otlp_exporter)
+    #tracer_provider.add_span_processor(span_processor)
 
     # Initialize Flask app
-    FlaskInstrumentor().instrument_app(app)
-    print("*" * 50)
-    print("Hello from application", end = " ")
-    print(app)
-    print("*" * 50)
-
-    # Imitate loading process
-    print("Loading application...")
-    for i in range(10, 0, -1):
-        print(f"Starting in {i} seconds...")
-        time.sleep(1)
-    print("Application loaded!")
+    #print("Application loaded!")
 
     # Start the Flask app
     port = int(os.getenv("FLASK_APP_PORT", 8080))
