e7ba1ead0e38bd08d4c9898120de2642f3d4f48c
Roman <pugachev.ra@yandex.ru>
2024-10-22 19:58:37 +0300

    Update swarm-check service

--- swarm-check-service/src/main/check-service/MainClientChecker.py
+++ swarm-check-service/src/main/check-service/MainClientChecker.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, render_template, request
 from Backends import LokiInteraction
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Base application parameters:
 @app.route('/')
